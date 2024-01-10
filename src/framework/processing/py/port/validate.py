2889b38580dd3c3456179dc7ee1bcc6728e03f38
trbKnl <ncdeschipper@gmail.com>
2024-01-10 13:32:26 +0100

    rebase and rework of validate.py

--- src/framework/processing/py/port/validate.py
+++ src/framework/processing/py/port/validate.py
@@ -48,39 +48,19 @@ class StatusCode:
 class ValidateInput:
     """
     Class containing the results of input validation
-
-    A validation class to store the result of the validation
-
-    status_codes: a list of all StatusCode's that are possible for a specific platform
-    ddp_categories: a list of all DDPCategory's that are possible for a specific platform
-    status_code: the status code of a submission after validation
-    ddp_category: the inferred DDP category after validation
     """
 
     status_codes: list[StatusCode]
     ddp_categories: list[DDPCategory]
-    status_code: StatusCode = StatusCode(-1, description="DDP not validated", message="")
-    ddp_category: DDPCategory = DDPCategory()
+    status_code: StatusCode | None = None
+    ddp_category: DDPCategory | None = None
 
     ddp_categories_lookup: dict[str, DDPCategory] = field(init=False)
     status_codes_lookup: dict[int, StatusCode] = field(init=False)
 
-    def set_status_code(self, id: int) -> None:
-        """
-        Set the status code according to a StatusCode id
-        """
-        self.status_code = self.status_codes_lookup[id]
-
-    def set_ddp_category(self, id: str) -> None:
-        """
-        Set the ddp_category code according to a DDPCategory id
-        """
-        self.ddp_category = self.ddp_categories_lookup[id]
-
     def infer_ddp_category(self, file_list_input: list[str]) -> bool:
         """
-        Compares a list of filenames to a list of known filenames.
-
+        Compares a list of files to a list of known files.
         From that comparison infer the DDP Category
         Note: at least 5% percent of known files should match
         """
@@ -90,7 +70,6 @@ class ValidateInput:
                 1 if f in category.known_files else 0 for f in file_list_input
             ]
             prop_category[identifier] = sum(n_files_found) / len(category.known_files) * 100
-            logger.debug("propertion of ddp categories: %s", prop_category)
 
         if max(prop_category.values()) >= 5:
             highest = max(prop_category, key=prop_category.get)  # type: ignore
@@ -100,6 +79,12 @@ class ValidateInput:
 
         logger.info("Not a valid input; not enough files matched when performing input validation")
         return False
+
+    def set_status_code(self, code: int) -> None:
+        """
+        Set the status code
+        """
+        self.status_code = self.status_codes_lookup.get(code, None)
 
     def __post_init__(self) -> None:
         for status_code, ddp_category in zip(self.status_codes, self.ddp_categories):
