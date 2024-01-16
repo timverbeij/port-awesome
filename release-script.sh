ea0fa6c4c3c61e7ede1dbae7bd669d1945f53571
trbKnl <ncdeschipper@gmail.com>
2024-01-16 16:21:49 +0100

    release script to build all platforms individually

--- release-script.sh
+++ release-script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# release all individual platforms
-script_location = ./src/framework/processing/py/port/script.py
+# build release all individual platforms
 
-all_platforms='platforms = \[ ("LinkedIn", extract_linkedin, linkedin.validate), ("Insta'
-all_platforms_commented_out='#platforms = \[ ("LinkedIn", extract_linkedin, linkedin.validate), ("Insta'
+script_location='./src/framework/processing/py/port/script.py'
+single_platform='platforms = \[ ("'
+single_platform_commented_out='#platforms = \[ ("'
 
-# comment out all platforms in sequence
-sed -i "s/$all_platforms/$all_platforms_commented_out/g" $script_location
+
+platforms=("LinkedIn" "Instagram" "Chrome" "Facebook" "Youtube" "TikTok" "Twitter")
+platforms=("LinkedIn")
+
+for platform in "${platforms[@]}"; do
+    sed -i "s/$single_platform_commented_out$platform/$single_platform$platform/g" $script_location
+    PLATFORM=$platform npm run release_platform
+    git restore $script_location
+done
